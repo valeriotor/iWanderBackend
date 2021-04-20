012d2f478846bd000ca37b459a220e739c3cd2bc
Valeriotor <valeriotorsiello@libero.it>
2021-04-20 21:26:11 +0200

    Added JedisPool bean

---
+++ src/main/java/com/valeriotor/iWanderBackend/config/JedisConfig.java
@@ -0,0 +1,20 @@
+package com.valeriotor.iWanderBackend.config;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+public class JedisConfig {
+
+    @Bean
+    public JedisPool jedisPool() {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(128);
+
+        return new JedisPool(poolConfig, "localhost", 6379, 8000);
+    }
+
+}
